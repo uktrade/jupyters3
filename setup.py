d4277516d9f9434e1d00ee485fc72b8cfb75565f
Michal Charemza <michal@charemza.name>
2019-08-12 09:22:04 +0100

    (release) v0.0.37

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='jupyters3',
-    version='0.0.36',
+    version='0.0.37',
     author='Department for International Trade - WebOps',
     author_email='webops@digital.trade.gov.uk',
     description='Jupyter Notebook Contents Manager for AWS S3',
