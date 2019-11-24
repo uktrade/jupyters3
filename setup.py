6b315e04e5c80a55468ea8ddc9a5d84b5cc7867d
Michal Charemza <michal@charemza.name>
2019-11-24 12:17:24 +0000

    (release) v0.0.40

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='jupyters3',
-    version='0.0.39',
+    version='0.0.40',
     author='Department for International Trade - WebOps',
     author_email='webops@digital.trade.gov.uk',
     description='Jupyter Notebook Contents Manager for AWS S3',
